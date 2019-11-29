bf65e514b4517959f3de1757fc7e7ee503ea4a99
amoghvnaik <amoghvnaik@yahoo.com>
2019-11-29 15:24:42 +0000

    commit

---
+++ exoplanets/application/models.py
@@ -0,0 +1,19 @@
+from application import db
+
+class Posts(db.Model):
+    planet_id = db.Column(db.Integer, primary_key=True, unique=True)
+    name = db.Column(db.String(20), nullable=False)
+    year_of_discovery = db.Column(db.Integer, nullable=False)
+    mass = db.Column(db.Float(10,2), nullable=False)
+    radius = db.Column(db.Float(10,2), nullable=False)
+    semi_major_axis = db.Column(db.Float(10,2), nullable=False)
+
+    def __repr__(self):
+        return ''.join([
+            'Name: ', self.name,
+            'Year of Discovery: ', self.year_of_discovery,
+            'Mass: ', self.mass,
+            'Radius: ', self.radius,
+            'Semi-major Axis: ', self.semi_major_axis
+        ])
+
---
+++ exoplanets/application/routes.py
@@ -0,0 +1,24 @@
+from flask import render_template
+from application import app
+from application.models import Posts
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    postData = Posts.query.all()
+    return render_template('home.html', title='Home', posts=postData)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login')
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title='Register')
+
+
