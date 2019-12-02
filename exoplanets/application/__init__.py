c8a8599717bef50f54de68bce3dbd978d5715953
amoghvnaik <amoghvnaik@yahoo.com>
2019-12-02 17:13:16 +0000

    commit

--- exoplanets/application/__init__.py
+++ exoplanets/application/__init__.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://"+os.getenv("MYSQL_USER")+":"+os.getenv("MYSQL_PASSWORD")+"@"+os.getenv("MYSQL_URL")+"/"+os.getenv("MYSQL_DB")
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://"+os.getenv("MYSQL_USER")+":"+os.getenv("MYSQL_PASSWORD")+"@"+os.getenv("MYSQL_IP")+"/"+os.getenv("MYSQL_DB")
 app.config["SECRET_KEY"] = os.getenv("MYSQL_KEY")
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
